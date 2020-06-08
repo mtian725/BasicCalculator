7c27c09288baa0fc57eef1da44fcb7e41496cf4d
merrickt <39343323+merrickt@users.noreply.github.com>
2020-06-08 01:10:11 -0400

    Create num.js

---
+++ src/num.js
@@ -0,0 +1,9 @@
+export class Num extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return <button type="button" className="num">{this.props.num}</button>
+  }
+}
