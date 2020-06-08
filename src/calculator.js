856a6cd2e9c4b399cbc3f779a2720ad569f9bf48
merrickt <39343323+merrickt@users.noreply.github.com>
2020-06-07 23:06:29 -0400

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -7,13 +7,25 @@ class Calculator extends React.Component {
           <p>something</p>
         </div>
         <div className="items">
+          <button type="button" className="temp-button">C</button>
+          <button type="button" className="temp-button">+/-</button>
+          <button type="button" className="temp-button">%</button>
+          <button type="button" className="temp-button">/</button>
+          <button type="button" className="temp-button">7</button>
+          <button type="button" className="temp-button">8</button>
+          <button type="button" className="temp-button">9</button>
+          <button type="button" className="temp-button">x</button>
+          <button type="button" className="temp-button">4</button>
+          <button type="button" className="temp-button">5</button>
+          <button type="button" className="temp-button">6</button>
+          <button type="button" className="temp-button">-</button>
           <button type="button" className="temp-button">1</button>
           <button type="button" className="temp-button">2</button>
           <button type="button" className="temp-button">3</button>
-          <button type="button" className="temp-button">4</button>
-          <button type="button" className="temp-bottom-a">5</button>
-          <button type="button" className="temp-bottom-b">6</button>
-          <button type="button" className="temp-bottom-b">7</button>
+          <button type="button" className="temp-button">+</button>
+          <button type="button" className="temp-bottom-a">0</button>
+          <button type="button" className="temp-bottom-b">.</button>
+          <button type="button" className="temp-bottom-b">=</button>
         </div>
       </div>
     );
