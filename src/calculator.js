eace19a828a173488e86463a6e70f6c3f33331ac
merrickt <39343323+merrickt@users.noreply.github.com>
2020-06-07 22:30:52 -0400

    Update calculator.js

--- src/calculator.js
+++ src/calculator.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../styles.css';
+import '../styles.css'
 
 class Calculator extends React.Component {
   render() {
     return (
       <div>
-        <div style="screen">
+        <div className="screen">
           <p>something</p>
         </div>
-        <div style="items">
-          <button type="button" style="temp-button">1</button>
-          <button type="button" style="temp-button">2</button>
-          <button type="button" style="temp-button">3</button>
-          <button type="button" style="temp-button">4</button>
-          <button type="button" style="temp-bottom-a">5</button>
-          <button type="button" style="temp-bottom-b">6</button>
-          <button type="button" style="temp-bottom-b">7</button>
+        <div className="items">
+          <button type="button" className="temp-button">1</button>
+          <button type="button" className="temp-button">2</button>
+          <button type="button" className="temp-button">3</button>
+          <button type="button" className="temp-button">4</button>
+          <button type="button" className="temp-bottom-a">5</button>
+          <button type="button" className="temp-bottom-b">6</button>
+          <button type="button" className="temp-bottom-b">7</button>
         </div>
       </div>
     );
