20b96207037ec44e111b46d580d2524cae414e34
merrickt <39343323+merrickt@users.noreply.github.com>
2020-06-08 01:11:30 -0400

    Delete num.js

--- src/num.js
+++
@@ -1,9 +0,0 @@
-export class Num extends React.Component {
-  constructor (props) {
-    super(props);
-  }
-
-  render () {
-    return <button type="button" className="num">{this.props.num}</button>
-  }
-}
