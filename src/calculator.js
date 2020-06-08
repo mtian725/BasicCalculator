de87fc5844ff47797569dd819fd329f4148e1450
merrickt <39343323+merrickt@users.noreply.github.com>
2020-06-07 23:26:24 -0400

    first row

--- src/calculator.js
+++ src/calculator.js
@@ -7,48 +7,48 @@ class Calculator extends React.Component {
           <p>something</p>
         </div>
         <div className="items">
-          <div className="grey-button">
-            <button type="button" className="temp-button">C</button>
-            <button type="button" className="temp-button">+/-</button>
-            <button type="button" className="temp-button">%</button>
+          <button type="button" className="button-a">
+            <div className="grey-button">C</div></button>
+          <button type="button" className="button-a">
+            <div className="grey-button">+/-</div></button>
+          <button type="button" className="button-a">
+            <div className="grey-button">%</div></button>
+          <button type="button" className="button-a">
+            <div className="orange-button">/</div></button>
+
+          <div className="black-button">
+            <button type="button" className="button-a">7</button>
+            <button type="button" className="button-a">8</button>
+            <button type="button" className="button-a">9</button>
           </div>
           <div className="orange-button">
-            <button type="button" className="temp-button">/</button>
+            <button type="button" className="button-a">x</button>
           </div>
 
           <div className="black-button">
-            <button type="button" className="temp-button">7</button>
-            <button type="button" className="temp-button">8</button>
-            <button type="button" className="temp-button">9</button>
+            <button type="button" className="button-a">4</button>
+            <button type="button" className="button-a">5</button>
+            <button type="button" className="button-a">6</button>
           </div>
           <div className="orange-button">
-            <button type="button" className="temp-button">x</button>
+            <button type="button" className="button-a">-</button>
           </div>
 
           <div className="black-button">
-            <button type="button" className="temp-button">4</button>
-            <button type="button" className="temp-button">5</button>
-            <button type="button" className="temp-button">6</button>
+            <button type="button" className="button-a">1</button>
+            <button type="button" className="button-a">2</button>
+            <button type="button" className="button-a">3</button>
           </div>
           <div className="orange-button">
-            <button type="button" className="temp-button">-</button>
+            <button type="button" className="button-a">+</button>
           </div>
 
           <div className="black-button">
-            <button type="button" className="temp-button">1</button>
-            <button type="button" className="temp-button">2</button>
-            <button type="button" className="temp-button">3</button>
+            <button type="button" className="b-bottom-a">0</button>
+            <button type="button" className="b-bottom-b">.</button>
           </div>
           <div className="orange-button">
-            <button type="button" className="temp-button">+</button>
-          </div>
-
-          <div className="black-button">
-            <button type="button" className="temp-bottom-a">0</button>
-            <button type="button" className="temp-bottom-b">.</button>
-          </div>
-          <div className="orange-button">
-            <button type="button" className="temp-bottom-b">=</button>
+            <button type="button" className="b-bottom-b">=</button>
           </div>
         </div>
       </div>
