bed808d19687c8e0d73e2d63ed739e71cd61f3b9
merrickt <39343323+merrickt@users.noreply.github.com>
2020-06-08 19:08:02 -0400

    testing +

--- src/calculator.js
+++ src/calculator.js
@@ -42,19 +42,37 @@ class Op extends React.Component {
   }
 
   render () {
-    if (this.props.op !== "=") {
-      return (
-        <button type="button" className="func" onClick={this.toggleOperation}>
-          {this.props.op}
-        </button>
-      );
+    if (this.props.operator === this.props.op) {
+      if (this.props.op !== "=") {
+        return (
+          <button type="button" className="func-select" onClick={this.toggleOperation}>
+            {this.props.op}
+          </button>
+        );
+      }
+      else {
+        return (
+          <button type="button" className="func-b-select" onClick={this.toggleOperation}>
+            {this.props.op}
+          </button>
+        );
+      }
     }
     else {
-      return (
-        <button type="button" className="func-b" onClick={this.toggleOperation}>
-          {this.props.op}
-        </button>
-      );
+      if (this.props.op !== "=") {
+        return (
+          <button type="button" className="func" onClick={this.toggleOperation}>
+            {this.props.op}
+          </button>
+        );
+      }
+      else {
+        return (
+          <button type="button" className="func-b" onClick={this.toggleOperation}>
+            {this.props.op}
+          </button>
+        );
+      }
     }
   }
 }
@@ -156,6 +174,11 @@ class Calculator extends React.Component {
 
   calcOp(op) {
     console.log(op);
+    if (op === "+") {
+      this.setState({
+        operator: "+"
+      });
+    }
   }
 
   // keep in mind that some math (division so far) isnt exact and can result in
@@ -196,26 +219,26 @@ class Calculator extends React.Component {
           <SpOp spOp="C" onClick={this.clearVal} />
           <SpOp spOp="+/-" onClick={this.toggleParity} />
           <SpOp spOp="%" onClick={this.addHundredths} />
-          <Op op="/" onClick={this.calcOp} />
+          <Op op="/" active={this.state.operator} onClick={this.calcOp} />
 
           <Num digit="7" onClick={this.addNum} />
           <Num digit="8" onClick={this.addNum} />
           <Num digit="9" onClick={this.addNum} />
-          <Op op="x" onClick={this.calcOp} />
+          <Op op="x" active={this.state.operator} onClick={this.calcOp} />
 
           <Num digit="4" onClick={this.addNum} />
           <Num digit="5" onClick={this.addNum} />
           <Num digit="6" onClick={this.addNum} />
-          <Op op="-" onClick={this.calcOp} />
+          <Op op="-" active={this.state.operator} onClick={this.calcOp} />
 
           <Num digit="1" onClick={this.addNum} />
           <Num digit="2" onClick={this.addNum} />
           <Num digit="3" onClick={this.addNum} />
-          <Op op="+" onClick={this.calcOp} />
+          <Op op="+" active={this.state.operator} onClick={this.calcOp} />
 
           <Num digit="0" onClick={this.addNum} />
           <Dec onClick={this.addDec}/>
-          <Op op="=" onClick={this.calcOp} />
+          <Op op="=" active={this.state.operator} onClick={this.calcOp} />
         </div>
       </div>
     );
