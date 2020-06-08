5027d75ff00e5cd8bf0f7a48b75121a967823593
mtian725 <mtian725@gmail.com>
2020-06-07 22:47:48 -0400

    convert

---
+++ temp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var temp = React.createElement(
+  'p',
+  null,
+  'Something'
+);
+
+ReactDOM.render(temp, document.getElementById('temp'));
