2199eed23a22400adaec1f2412f363dd761e9e0a
merrickt <39343323+merrickt@users.noreply.github.com>
2020-06-07 23:44:53 -0400

    css edited

--- src/calculator.js
+++ src/calculator.js
@@ -7,49 +7,29 @@ class Calculator extends React.Component {
           <p>something</p>
         </div>
         <div className="items">
-          <button type="button" className="button-a">
-            <div className="grey-button">C</div></button>
-          <button type="button" className="button-a">
-            <div className="grey-button">+/-</div></button>
-          <button type="button" className="button-a">
-            <div className="grey-button">%</div></button>
-          <button type="button" className="button-a">
-            <div className="orange-button">/</div></button>
+          <button type="button" className="special-func">C</button>
+          <button type="button" className="special-func">+/-</button>
+          <button type="button" className="special-func">%</button>
+          <button type="button" className="func">/</button>
 
-          <div className="black-button">
-            <button type="button" className="button-a">7</button>
-            <button type="button" className="button-a">8</button>
-            <button type="button" className="button-a">9</button>
-          </div>
-          <div className="orange-button">
-            <button type="button" className="button-a">x</button>
-          </div>
+          <button type="button" className="num">7</button>
+          <button type="button" className="num">8</button>
+          <button type="button" className="num">9</button>
+          <button type="button" className="func">x</button>
 
-          <div className="black-button">
-            <button type="button" className="button-a">4</button>
-            <button type="button" className="button-a">5</button>
-            <button type="button" className="button-a">6</button>
-          </div>
-          <div className="orange-button">
-            <button type="button" className="button-a">-</button>
-          </div>
+          <button type="button" className="num">4</button>
+          <button type="button" className="num">5</button>
+          <button type="button" className="num">6</button>
+          <button type="button" className="func">-</button>
 
-          <div className="black-button">
-            <button type="button" className="button-a">1</button>
-            <button type="button" className="button-a">2</button>
-            <button type="button" className="button-a">3</button>
-          </div>
-          <div className="orange-button">
-            <button type="button" className="button-a">+</button>
-          </div>
+          <button type="button" className="num">1</button>
+          <button type="button" className="num">2</button>
+          <button type="button" className="num">3</button>
+          <button type="button" className="func">+</button>
 
-          <div className="black-button">
-            <button type="button" className="b-bottom-a">0</button>
-            <button type="button" className="b-bottom-b">.</button>
-          </div>
-          <div className="orange-button">
-            <button type="button" className="b-bottom-b">=</button>
-          </div>
+          <button type="button" className="num-z">0</button>
+          <button type="button" className="decimal">.</button>
+          <button type="button" className="func-b">=</button>
         </div>
       </div>
     );
