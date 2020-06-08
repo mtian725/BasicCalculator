ea2c49f5893758f35cef58061de9f9780c134097
merrickt <39343323+merrickt@users.noreply.github.com>
2020-06-08 13:18:55 -0400

    append num added

--- src/calculator.js
+++ src/calculator.js
@@ -3,26 +3,20 @@
 class Num extends React.Component {
   constructor (props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  // temp to check that it works
-  handleChange() {
-    const newNum = this.props.digit;
-    this.props.onClick(newNum);
+    this.appendNum = this.appendNum.bind(this);
   }
 
   render () {
     if (this.props.digit !== 0) {
       return (
-        <button type="button" className="num" onClick={this.handleChange}>
+        <button type="button" className="num" onClick={this.appendNum}>
           {this.props.digit}
         </button>
       );
     }
     else {
       return (
-        <button type="button" className="num-z" onClick={this.handleChange}>
+        <button type="button" className="num-z" onClick={this.appendNum}>
           {this.props.digit}
         </button>
       );
@@ -66,16 +60,23 @@ class Calculator extends React.Component {
   constructor (props) {
     super(props);
     this.state = { // will change state names to make it work with everything just val for now for testing purposes
-      val: 0
+      val: "0"
     };
-    this.changeVal = this.changeVal.bind(this);
+    this.addNum = this.addNum.bind(this);
   }
 
-  //temporary to check to make sure buttons work
-  changeVal(newVal) {
+  addNum(digit) {
+    const curVal = this.state.val;
+    if (curVal === "0") {
       this.setState({
-        val: newVal
+        val: digit
       });
+    }
+    else {
+      this.setState({
+        val: (curVal + digit)
+      });
+    }
   }
 
   render () {
@@ -90,22 +91,22 @@ class Calculator extends React.Component {
           <SpOp spOp="%" />
           <Op op="/"/>
 
-          <Num digit={7} onClick={this.changeVal} />
-          <Num digit={8} onClick={this.changeVal} />
-          <Num digit={9} onClick={this.changeVal} />
+          <Num digit="7" onClick={this.addNum} />
+          <Num digit="8" onClick={this.addNum} />
+          <Num digit="9" onClick={this.addNum} />
           <Op op="x"/>
 
-          <Num digit={4} onClick={this.changeVal} />
-          <Num digit={5} onClick={this.changeVal} />
-          <Num digit={6} onClick={this.changeVal} />
+          <Num digit="4" onClick={this.addNum} />
+          <Num digit="5" onClick={this.addNum} />
+          <Num digit="6" onClick={this.addNum} />
           <Op op="-"/>
 
-          <Num digit={1} onClick={this.changeVal} />
-          <Num digit={2} onClick={this.changeVal} />
-          <Num digit={3} onClick={this.changeVal} />
+          <Num digit="1" onClick={this.addNum} />
+          <Num digit="2" onClick={this.addNum} />
+          <Num digit="3" onClick={this.addNum} />
           <Op op="+"/>
 
-          <Num digit={0} onClick={this.changeVal} />
+          <Num digit="0" onClick={this.addNum} />
           <Dec />
           <Op op="="/>
         </div>
